d47895a7a27ef5d5100fe3310d099d4972b5816d
Sebastian <huajinglu0312@gmail.com>
2023-11-21 03:33:03 -0800

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json());
 app.use(cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        //origin: process.env.FRONTEND_URL
     }
 ));
 const sessionOptions = {
